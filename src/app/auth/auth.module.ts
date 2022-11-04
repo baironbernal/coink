cc68116d78f4a785f9ff1b95a1a539d38bfcedc6
Bairon Bernal <baironbernal263@gmail.com>
2022-11-04 13:20:14 -0500

    advance

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CharacterComponent } from './character/character.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    CharacterComponent
   ],
   imports: [
     CommonModule,
