f4c1d28430f6a467617ac553e4c134df02380a44
Bairon Bernal <baironbernal263@gmail.com>
2022-11-04 09:34:57 -0500

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+      }
+    ]
+  },
+  {  path: '**', redirectTo: 'battle' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
