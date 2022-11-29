80f4a4283b8d9e11830872adae684184ef81d229
Bairon Bernal <bairon.bernal@one-click.com.co>
2022-11-28 20:32:49 -0500

    advance

--- src/app/auth/services/character.service.ts
+++ src/app/auth/services/character.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Character } from '../interfaces/character.interface';
 import { environment } from '@environment/environment';
+import { Observable, pluck } from 'rxjs';
 
 
 @Injectable({
@@ -11,7 +12,7 @@ export class CharacterService {
 
   constructor(private http: HttpClient) { }
 
-  getCharacters(name = '', page = null) {
+  getCharacters(name = '', page = null): Observable<any> {
     const filter = `${environment.urlAPI}/?page=${page}&name=${name}`;
     return this.http.get<Character[]>(filter);
   }
